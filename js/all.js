e676f4bc1eceeaeca00348e8662bfe31ee78c228
Groverjun <1250901900@qq.com>
2017-10-18 17:38:15 +0800

    add

--- js/all.js
+++ js/all.js
@@ -8,6 +8,9 @@ $(".mat-button").on("click", function() {
 		$("#left").css("left", "0")
 	}
 })
+$(".container").on("click",function(){
+	$("#left").css("left", "-250px");
+})
 $(".left_list li").on("click", function() {
 	$(this).css("background", "rgba(0,0,0,0.2)").siblings().css("background", "none")
 	$(this).find("em").css("transform", "rotate(0deg)").parent().parent().siblings().find("em").css("transform", "rotate(-90deg)")
